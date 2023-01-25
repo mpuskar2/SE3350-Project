0801bdc45fe63bfd38113d7147f5cf61d572f863
Michael Ruiz <mruiz6@uwo.ca>
2023-01-25 17:45:50 -0500

    added login template

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -2,6 +2,20 @@ import React, { useState } from 'react';
 
 export default function Login() {
   return (
-    <div>Login</div>
+    <div className="login">
+      <ul>
+        <li>
+        <label>Username </label>
+        <input type="text"></input>
+        </li>
+        <li>
+        <label>Password </label>
+        <input type="password"></input>
+        </li>
+        <li>
+          <button>Submit</button>
+        </li>
+      </ul>
+    </div>
   )
 }
