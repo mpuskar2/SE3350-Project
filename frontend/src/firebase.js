33016bbfc201e3e024c1df0faf317b897941b5f0
Michael Ruiz <mruiz6@uwo.ca>
2023-02-15 18:01:13 -0500

    setup database in firebse file

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -1,5 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
+import { getDatabase, set, ref} from "firebase/database";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAQGHrc_OgGqpK_sLXFIljlAlX8HWkUrQo",
@@ -8,9 +9,15 @@ const firebaseConfig = {
   storageBucket: "course-outline-manager.appspot.com",
   messagingSenderId: "787623733149",
   appId: "1:787623733149:web:6115dac376f09bd089b174",
-  measurementId: "G-667WRYR7PW"
+  measurementId: "G-667WRYR7PW",
+  databaseURL: "https://course-outline-manager-default-rtdb.firebaseio.com"
 };
-  
+
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-export default auth;
+export const db = getDatabase(app);
+export const addOutline = (id, courseName, whoModified, modifiedTime, versionNum, approvalStatus, filePath ) => {
+  set(ref(db, `Outlines/${id}`), {courseName:courseName, whoModified:whoModified, 
+    modifiedTime:modifiedTime, versionNum:versionNum, approvalStatus:approvalStatus, filePath:filePath});
+}
+export const auth = getAuth(app);
+export default app;
